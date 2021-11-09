980b38fc7f4256466271f519013d3b9099dd9194
evewolfs <53898640+evewolfs@users.noreply.github.com>
2021-11-09 03:33:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,34 +24,54 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>home</title>
+    
+    <script src="https://global.localizecdn.com/localize.js"></script>
+<script>!function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","getSourceLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off","hideWidget","showWidget"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);</script>
+
+<script>
+  Localize.initialize({
+    key: 'RleGNc5cAS1Xw',
+    rememberLanguage: true,
+  });
+</script>
+
+<!-- parent localize -->
+<script src="https://global.localizecdn.com/localize.js"></script>
+    <script>!function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off","hideWidget","showWidget","getSourceLanguage"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);</script>
+
+    <script>
+      Localize.on("setLanguage", function(data) {
+        // Pass the data object onto the iframe with id="localize_iframe" so it can change the current language to match the parent language.
+        document.querySelector('iframe[id="localize_iframe"]').contentWindow.postMessage(data, '*');
+      });
+
+      Localize.initialize({
+         key: 'RleGNc5cAS1Xw',  
+        rememberLanguage: true,
+        // other options go here, separated by commas
+      });
+    </script>
   </head>
   <body>   
+
                  <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MG3RRWK"
+<noscript><iframe id="localize_iframe" src="https://www.googletagmanager.com/ns.html?id=GTM-MG3RRWK"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->  
      <br/><br/> 
-     <center>
-     <h2>Website under maintenance,<br/> please come back later or contact us</h2>
-     <br/>
-     <a href="mailto:kinostelux@gmail.com">
-                  <div class="doctor-icon-container">
-                     <div class="icon-box">
-                        <i class="bi bi-envelope"></i>
-                           <h6>EMAIL</h6>
-                     </div>
-                  </div>
-                  
-               </a>
-               <a href="tel:0035224873092">
-                  <div class="doctor-icon-container">
-                     <div class="icon-box">
-                        <i class="bi bi-telephone"></i>
-                           <h6>+352 24 87 30 92 </h6>
-                     </div>
-                  </div>
-               </a>
-               </center>
+
+
+               <form name="form" >
+    <select size="1" id="select" onChange="location = this.options[this.selectedIndex].value;">
+  
+    <option selected="true" disabled="disabled">Prendre rendez-vous</option> 
+      <option value="./gregorytest.php" >Gregory Berghman</option>
+      <option value="./cabinettest.php" >Toutes les disponibilités</option>
+     
+    </select>
+
+  </form>
+
 
  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
